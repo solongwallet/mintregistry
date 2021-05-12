7e4f2f6f88b93f366ce2c440f4b346e23f948914
CZ <cz.theng@gmail.com>
2021-05-12 11:05:41 +0800

    [VER] 0.0.7

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -1,4 +1,5 @@
 // @flow
 export {MintRegistry, MintExtension} from './mint_registry';
 export {MintExtensionLayout} from './layout';
-export const ProgramID = "FKNCdZBzWwX3oSek1ubTu1oFunj7MVCca5i8PDMD7LNi";
+//export const ProgramID = "FKNCdZBzWwX3oSek1ubTu1oFunj7MVCca5i8PDMD7LNi";
+export const ProgramID = "4pqbBJvuAf3Kguq2rUipT2QQAVLM5HEJepc8AHpF5yi6";
